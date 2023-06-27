8b1f87b52bd8441a652de53f5b0e2e29ecaf8da7
Emmanuel Lyimo <esal29t@gmail.com>
2023-06-27 05:00:41 -0700

    variables as used in python

--- index.py
+++ index.py
@@ -2,3 +2,11 @@ print('Hello World!')
 print('Create Hummer!')
 print('Create Nails')
 print('Use Hummer and Nails to create')
+
+# Variables
+failed_subjects = " 6",
+print('Dear Mrs Lyimo')
+print('Your son is failing' + failed_subjects + ' subjects')
+print('He will have to redo' + failed_subjects + ' subjects')
+print('But you son is good in Math')
+
