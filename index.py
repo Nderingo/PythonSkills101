cfb81a88da9a2204002bfb0d0e6e47b15156c4d1
Emmanuel Lyimo <esal29t@gmail.com>
2023-06-29 01:03:37 -0700

    introduction to data types

--- index.py
+++ index.py
@@ -12,3 +12,12 @@ print( name + ' will have to redo' + failed_subjects + ' subjects')
 name="Richard"
 print('But ' + name + ' is good in Math')
 
+# Naming of variables in python should be in one word using underscore or using camel cases
+# strings = are asigned with either "" or ''
+
+# Data types
+
+# Integers = They are whole numbers 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+# Floats = These are decimal numbers 1.2, 3.6 etc
+# Boolean = These are data that can take the value of true or false
+
