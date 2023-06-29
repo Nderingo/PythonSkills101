e2039c69b62d8b53be755adfa4bdfa8ac28dd349
Emmanuel Lyimo <esal29t@gmail.com>
2023-06-29 00:36:36 -0700

    working with variables

--- index.py
+++ index.py
@@ -4,9 +4,10 @@ print('Create Nails')
 print('Use Hummer and Nails to create')
 
 # Variables
-failed_subjects = " 6",
+failed_subjects = " 6"
+name="John"
 print('Dear Mrs Lyimo')
-print('Your son is failing' + failed_subjects + ' subjects')
-print('He will have to redo' + failed_subjects + ' subjects')
-print('But you son is good in Math')
+print(name + ' is failing' + failed_subjects + ' subjects')
+print( name + ' will have to redo' + failed_subjects + ' subjects')
+print('But ' + name + ' is good in Math')
 
